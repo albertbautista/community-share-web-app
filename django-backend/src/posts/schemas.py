7d16ccdf054a2e15c96dc8df6f436b0fa80fc3c1
Albert Bautista <abautista95@toromail.csudh.edu>
2026-02-27 18:04:38 -0800

    Implemented login and registration features. Added author field to Post model which links a Post to a user.  Modfied specific posts api endpoints to require authentication.

--- django-backend/src/posts/schemas.py
+++ django-backend/src/posts/schemas.py
@@ -1,19 +1,26 @@
-from ninja import Schema
+from ninja import Schema, Field
 from datetime import datetime
 
 # Schemas for endpoint parameters would go here.
 
-# PostInputSchema defines the expected format for posts, it validates the data (input) being sent by the user.
+# InputSchemas define the expected format for posts, it validates the data (input) being sent by the user.
+
+# OutputSchemas define what the user sees, it allows us to control what the output to the user is.
+# E.g. we can hide or expose fields of our choosing for the user to see / not see
 
 class PostInputSchema(Schema):
     title: str
     content: str
 
-# PostOutputSchema defines what the user sees, it allows us to control what the output to the user is.
-# E.g. we can hide or expose fields of our choosing for the user to see / not see
+class AuthorSchema(Schema):
+    username: str
 
 class PostOutputSchema(Schema):
     id: int
     title: str
     content: str
+    author: AuthorSchema
     created_at: datetime
+
+class ErrorOutputSchema(Schema):
+    error: str
--- django-backend/src/users/api.py
+++ django-backend/src/users/api.py
@@ -2,13 +2,40 @@ from ninja import Router
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
 from ninja_jwt.tokens import RefreshToken
+from .schemas import RegisterInputSchema, LoginInputSchema, RegisterOutputSchema, TokenOutputSchema, ErrorOutputSchema
 
 # Endpoints here would route to the main api instance defined in src/config/api.py
 # so instead of using @api.get/post/put/delete use @router.get/post/put/delete instead
 
+# You can test endpoints at http://127.0.0.1:8000/api/docs  (Make sure server is running)
+
 router = Router()
 
-# We can use the built in User model to make new users (from django.contrib.auth.models import User).
+# Using the built in User model to make new users (from django.contrib.auth.models import User).
 
-# the users api would handle register and login, would also handle authentication here using "authenticate"
-# and generate tokens with "RefreshToken" after a successful login
+@router.post("/signup", response = {201: RegisterOutputSchema, 400: ErrorOutputSchema})
+def register(request, data: RegisterInputSchema):
+    if User.objects.filter(username = data.username).exists():
+        return 400, {"error": "Username already taken"} 
+    
+    # Creates a new user
+    user = User.objects.create_user (
+        email = data.email,
+        username = data.username,
+        password = data.password
+    )
+    return 201, user
+
+@router.post("/signin", response = {200: TokenOutputSchema, 401: ErrorOutputSchema})
+def login(request, data: LoginInputSchema):
+
+    user = authenticate(username = data.username, password = data.password)
+    if not user:
+        return 401, {"error": "Invalid credentials"} 
+    
+    # Generates token
+    refresh = RefreshToken.for_user(user)
+
+    return 200, { 
+        'access': str(refresh.access_token),
+    }
